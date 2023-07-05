4a6a70cce343a64929bbf8f38788ebba881c0a22
jakeeeyre <133030390+jakeeeyre@users.noreply.github.com>
2023-07-05 17:45:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-
 const numberRatingHolder = document.querySelector(".rating-wrapper");
-const navButton = document.querySelector("#submit-button")
+const navButton = document.querySelector("#submit-button");
 
-
-//change color when number is chosen, plus add chosen number to results page
-numberRatingHolder.addEventListener("click", e => {
-    const numberRating = Array.from(numberRatingHolder.children)
-    const choice = document.querySelector("#change")
-    const chosenNum = e.target.closest("p")
-    if (!chosenNum) return;
-    numberRating.forEach((num) => num.classList.remove("chosen-num"))
-    chosenNum.classList.add("chosen-num")
-    choice.innerHTML = chosenNum.innerHTML
-})    
+//change color when number is chosen then back if another number is selected
+numberRatingHolder.addEventListener("click", (e) => {
+	const numberRating = Array.from(numberRatingHolder.children);
+	const choice = document.querySelector("#change");
+	const chosenNum = e.target.closest("p");
+	if (!chosenNum) return;
+	numberRating.forEach((num) => num.classList.remove("chosen-num"));
+	chosenNum.classList.add("chosen-num");
+    //match rating to option chosen
+	choice.innerHTML = chosenNum.innerHTML;
+    navButton.innerHTML = "Submit"
+});
 
 //change to results page
 navButton.onclick = () => {
-    const unhideThanks = document.querySelector(".wrapper-thankyou")
-    const hideRating = document.querySelector(".wrapper")
-    unhideThanks.classList.remove("hidden");
-    hideRating.classList.add("hidden")
-}
+	const checkHasValue = document.querySelector("#change");
+    //check a number has been chosen
+	if (checkHasValue.innerHTML) {
+		const unhideThanks = document.querySelector(".wrapper-thankyou");
+		const hideRating = document.querySelector(".wrapper");
+		unhideThanks.classList.remove("hidden");
+		hideRating.classList.add("hidden");
+	} else {
+        navButton.innerHTML = "Please choose a number"
+    }
+};
